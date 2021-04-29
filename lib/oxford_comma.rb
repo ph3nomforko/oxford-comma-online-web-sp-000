fa109d8ab9c77054659487374b4b60203087e16b
Codi Kraus <codi.kraus@gmail.com>
2021-04-29 00:20:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
 def oxford_comma(array)
-  if array.size == 1
+  case array.length
+  when 1
     array.join
-  elsif array.size == 2
+  when 2
     array.join(" and ")
   else
-    array.each do |list_pos|
-      if list_pos == array.last
-        list_pos = "and" << list_pos
-      end
-    end
+    last_item = array[-1]
+    "and " = array[-1]
     array.join(", ")
   end
 end
